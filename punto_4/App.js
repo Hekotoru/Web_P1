2b617f6ac7b7dc186321a0b05c6cfa483599c2ff
Hector Aristy <rafelito266@gmail.com>
2017-01-23 19:46:18 -0400

    Puntos 1,2,3,5 realizados.

--- punto_4/App.js
+++ punto_4/App.js
@@ -1,9 +1,44 @@
 /*Repo: https://github.com/Hekotoru/Web_P1*/
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var port = 8081;
+var path = require('path');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/404', function(req, res) {
+	res.status(404).send();
+});
+
+app.get('/protected',function(req, res) {
+	res.status(401).send();
+});
+
+app.get('/error',function(req,res){
+	res.status(500).send();
+});
+
+app.get('/noimplemented',function(req,res){
+
+});
+
+app.get('/login',function(req, res) {
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.post('/login',function(req,res){
+	var username = req.body.username;
+	var password = req.body.password;
+
+	var jsonResponse = {
+		"username" : username,
+		"password" : password
+	}
+	res.type('json').status(200).send(jsonResponse);
+});
 
 app.get('*', function(req, res) {
 	var ports = req.get('host').split(":");
